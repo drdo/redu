6677c43652ddbd1ca284451a4920d516f07551df
Daniel Rebelo de Oliveira <drdo@drdo.eu>
2024-05-08 16:26:35 +0100

    move shorten_to to app module

--- src/component/app.rs
+++ src/component/app.rs
@@ -9,8 +9,9 @@ use ratatui::prelude::Line;
 use ratatui::style::{Style, Stylize};
 use ratatui::text::Span;
 use ratatui::widgets::{List, ListItem, Paragraph, WidgetRef};
+use unicode_segmentation::UnicodeSegmentation;
 
-use crate::component::{Action, Event, shorten_to};
+use crate::component::{Action, Event};
 use crate::types::{Directory, Entry, File};
 
 struct ListEntry {
@@ -319,5 +320,54 @@ mod tests {
             f.to_line(2),
             Line::raw(" 999.99 KiB [##############  ] ".to_owned())
         );
+    }
+}
+
+fn shorten_to(s: &str, width: usize) -> Cow<str> {
+    let len = s.graphemes(true).count();
+    let res = if len <= width {
+        Cow::Borrowed(s)
+    }
+    else if width <= 3 {
+        Cow::Owned(".".repeat(width))
+    } else {
+        let front_width = (width - 3).div_euclid(2);
+        let back_width = width - front_width - 3;
+        let graphemes = s.graphemes(true);
+        let mut name = graphemes.clone().take(front_width).collect::<String>();
+        name.push_str("...");
+        for g in graphemes.skip(len-back_width) { name.push_str(g); }
+        Cow::Owned(name)
+    };
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use std::borrow::Cow;
+    use super::shorten_to;
+
+    use super::*;
+
+    #[test]
+    fn shorten_to_len_lt_width() {
+        let s = "12345";
+        assert_eq!(shorten_to(s, 6), Cow::Borrowed(s));
+    }
+
+    #[test]
+    fn shorten_to_width_lt_3() {
+        let s = "12345";
+        assert_eq!(shorten_to(s, 2), Cow::Owned::<str>("..".to_owned()));
+    }
+
+    #[test]
+    fn shorten_to_width_lte_len() {
+        let s = "123456789";
+        assert_eq!(shorten_to(s, 3), Cow::Owned::<str>("...".to_owned()));
+        assert_eq!(shorten_to(s, 4), Cow::Owned::<str>("...9".to_owned()));
+        assert_eq!(shorten_to(s, 5), Cow::Owned::<str>("1...9".to_owned()));
+        assert_eq!(shorten_to(s, 8), Cow::Owned::<str>("12...789".to_owned()));
+        assert_eq!(shorten_to(s, 9), Cow::Owned::<str>("123456789".to_owned()));
     }
 }
