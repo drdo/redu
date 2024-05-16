bbf642fa0d81f94a59be79302106164c4294bdc7
Daniel Rebelo de Oliveira <drdo@drdo.eu>
2024-05-16 18:39:00 +0100

    fix benches

--- benches/cache.rs
+++ benches/cache.rs
@@ -3,9 +3,9 @@ use std::fs;
 use std::path::PathBuf;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use uuid::Uuid;
-use dorestic::cache::Cache;
+use redu::cache::{Cache, SnapshotGroup};
 
-use dorestic::cache::tests::*;
+use redu::cache::tests::*;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
 
@@ -15,15 +15,18 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         b.iter(move || filetree0.take().merge(black_box(filetree1.take())));
     });
  
-    c.bench_function("save filetree", |b| {
-        let filetree = generate_filetree(black_box(6), black_box(12));
-        let snapshot = "foo";
+    c.bench_function("create and save group", |b| {
         let file: PathBuf = Uuid::new_v4().to_string().into();
         {
             let mut cache = Cache::open(&file).unwrap();
-            b.iter(||
-                cache.save_snapshot(black_box(snapshot), black_box(&filetree))
-            );
+            b.iter(move || {
+                let mut group = SnapshotGroup::new();
+                group.add_snapshot(
+                    "foo".into()
+                    , generate_filetree(black_box(6), black_box(12))
+                );
+                cache.save_snapshot_group(group).unwrap()
+            });
         }
         fs::remove_file(&file).unwrap();
     });
